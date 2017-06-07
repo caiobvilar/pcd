c56490b2a62b82f6ade932728323e8959bce5b5d
caiobvilar <caio.b.vilar@gmail.com>
2017-06-07 16:35:24 -0300

    functions defined

--- feed.c
+++ feed.c
@@ -3,7 +3,8 @@
 #include <omp.h>
 
 
-void produtomatricial(float *in1,float *in2,float *out,neuronios,entradas+1,exemplos);
+void produtomatricial(float *in1,float *in2,float *out,int row1, int row2, int col1);
+void funcaoAtivacao(float *in,float *out,int row, int col);
 
 int main(int argc, char *argv[])
 {
@@ -15,9 +16,40 @@ int main(int argc, char *argv[])
 	produtomatricial(Wx[0],X[0],A[0],neuronios,entradas+1,exemplos);
 	funcaoAtivacao(A[0],Z[0],neuronios,exemplos);
 	produtomatricial(Wy[0],Z[0],C[0],saidas,neuronios+1,exemplos);
-
 	funcaoAtivacao(C[0],Y[0],saidas,exemplos);
+	return 0;
 }
 
 
+
+
+
+void produtomatricial(float *in1,float *in2,float *out,int row1, int col1, int col2)
+{
+  int i,j,k;
+  for(i=0; i<row; i++)
+  {
+    for(j=0; j<col; j++)
+    {
+      *out = 0;
+      for(k=0; k<row2; k++)
+      {
+        *out += (*(in1 + i*col1 + k))*(*(in2 + k*col2 + j));
+      }
+      out++;
+    }
+  }
+}
+
+void funcaoAtivacao(float *in,float *out,int row, int col)
+{
+	int i,j;
+	for(i=0;i < row;i++)
+	{
+		for(j = 0;j< col;j++)
+		{
+			out[i*col+j] = 1.0/(1.0 + exp(-(in[i*col+j])));
+		}
+	}
+}
 
