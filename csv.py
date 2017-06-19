2e4bbca6fe4d4917e2539d9eb4e9f2fcec084f7a
Miguel Rocha Jr - Ubuntu VM <miguelsrochajr@gmail.com>
2017-06-18 23:25:44 -0700

    updated code

--- csv.py
+++ csv.py
@@ -1,12 +1,13 @@
 # Para criar o arquivo, execute o codigo da seguinte forma:
-# python csv.py >> <filename>.txt
+# python csv.py > <filename>.txt
 
-a = "1111010101010101010100101100101010101010101010100101001010101010101010111100011010111001011"
-array = list(a)
+expectedValue = "00011011"
 
-for i in range(0,len(a),5):
+array = list(expectedValue)
+
+for i in range(0,len(expectedValue),2):
     try:
-        line="{},{},{},{},{}".format(a[i], a[i+1],a[i+2],a[i+3],a[i+4])
+        line="{},{}".format(expectedValue[i], expectedValue[i+1])
         print(line)
     except:
         print("FINISHED")
